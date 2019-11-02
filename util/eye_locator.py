0585c9eb5033388bc20d85dba4325d1ef11d2d81
NickR23 <NickRich2624@gmail.com>
2019-11-02 12:39:18 +0000

    Added eye paster

--- util/eye_locator.py
+++ util/eye_locator.py
@@ -18,6 +18,37 @@ def get_highlighted_eyes(img_path):
     return pil_img
 
 
+def get_poly_center(points):
+    x = [p[0] for p in points]
+    y = [p[1] for p in points]
+    center = (sum(x) / len(points), sum(y) / len(points))
+    center = [round(x) for x in center]
+    return center
+
+
+def get_pasted_eyes(img_path):
+    image = face_recognition.load_image_file(img_path)
+    lm = get_eye_poly(image)
+
+    face_img = Image.fromarray(image)
+
+    left_eye_position = get_poly_center(lm['left_eye'])
+    left_eye_img = Image.open('./assets/clear_eye.png', 'r')
+    left_eye_img.thumbnail((50, 50), Image.ANTIALIAS)
+    left_eye_position = (left_eye_position[0] - 25, left_eye_position[1] - 25)
+    face_img.paste(left_eye_img, left_eye_position, left_eye_img)
+
+    right_eye_position = get_poly_center(lm['right_eye'])
+    right_eye_img = Image.open('./assets/clear_eye.png', 'r')
+    right_eye_img.thumbnail((50, 50), Image.ANTIALIAS)
+    right_eye_position = (right_eye_position[0] - 25, right_eye_position[1] - 25)
+    face_img.paste(right_eye_img, right_eye_position, right_eye_img)
+
+    return face_img
+
+
 if __name__ == '__main__':
-    scary_danny = get_highlighted_eyes('./assets/frank.jpg')
-    scary_danny.save('./output/frank.jpg')
+    # scary_danny = get_highlighted_eyes('./assets/frank.jpg')
+    # scary_danny.save('./output/frank.jpg')
+    pasted_eye_img = get_pasted_eyes('./assets/frank.jpg')
+    pasted_eye_img.save('output/pasted.png')
